063365cafaadaf3a44a7bf7774c71d054e5b287d
Sébastien Frantz <sebastien.frantz@hei.fr>
2019-06-18 14:54:40 +0200

    add post method

--- server/services/index.js
+++ server/services/index.js
@@ -4,9 +4,23 @@ const sendJsonResponse = (res, statusCode, message) => {
     res.end(JSON.stringify(message));
 }
 
-export function sampleRequest(req, res) {
+export const sampleGetRequest = (req, res) => {
     const response = {
         "msg": "Hello"
     };
     sendJsonResponse(res, 200, response);
+}
+
+export const samplePostRequest = (req, res) => {
+    let body = '';
+    req.on('data', function (chunk) {
+        body += chunk;
+    });
+    req.on('end', function () {
+        const jsonBody = JSON.parse(body);
+        const response = {
+            "bodySent": jsonBody
+        }
+        sendJsonResponse(res, 200, response);
+    });
 }
